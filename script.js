55528a724d05bdf753277b009824d484585988f6
Bhoopal <bhoopal10@gmail.com>
2024-08-09 15:12:02 +0530

    updated changes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // script.js
 document.getElementById('smashObject').addEventListener('click', () => {
     const smashObject = document.getElementById('smashObject');
-    
     // Add class to trigger the smash animation
     smashObject.classList.add('hit');
+    smashObject.classList.remove('top');
     // smashObject.classList.remove('hit-back');
 
     // Reset the effect after animation completes
     setTimeout(() => {
         smashObject.classList.remove('hit');
+        smashObject.classList.add('top');
         // smashObject.classList.add('hit-back');
     }, 2000); // Duration of the effect, should match the CSS transition time
 });
